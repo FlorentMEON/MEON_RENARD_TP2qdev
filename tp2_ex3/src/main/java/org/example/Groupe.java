f910acc8d2f6fa2d0956834fd90d51e51280845f
Florent MEON <florent.meon6@etu.univ-lorraine.fr>
2026-09-25 09:16:37 +0200

    ajout de Groupe

---
+++ tp2_ex3/src/main/java/org/example/Groupe.java
@@ -0,0 +1,20 @@
+package main.java.org.example;
+
+import java.util.Set;
+
+public class Groupe {
+    private Set<Etudiant> etu;
+    Formation format;
+
+    public Groupe(Formation f) {
+        this.format = f;
+    }
+
+    public void ajouterEtudiant(Etudiant e) {
+
+    }
+
+    public void retirerEtudiant(Etudiant e)  {
+
+    }
+}
