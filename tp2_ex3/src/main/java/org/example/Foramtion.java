4bc46cc70c2986b42fc56dfe86e775bea836c76e
Florent MEON <florent.meon6@etu.univ-lorraine.fr>
2026-09-25 08:31:51 +0200

    ajout de formation

---
+++ tp2_ex3/src/main/java/org/example/Foramtion.java
@@ -0,0 +1,31 @@
+package main.java.org.example;
+
+import java.util.Map;
+
+public class Foramtion {
+    String identifiant;
+    Map<String,Double> matieres;
+
+    public Foramtion(String identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public void ajouterFormation(String matiere,double valeur) {
+        this.matieres.put(matiere,valeur);
+    }
+
+    public void retirerFormation(String matiere,double valeur) {
+        this.matieres.remove(matiere);
+    }
+
+    public String getCoef(String matiere) {
+        if(matieres.containsKey(matiere)){
+        return identifiant;
+        }else{
+            return null;
+        }
+    }
+
+
+
+}
